e3a905835d77891d1f44952fea4b0e1598717cd5
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-02-12 12:43:40 +0000

    working nicely

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
 
-var es      = require('event-stream');
-var fs      = require('fs');
+var es        = require('event-stream');
+var fs        = require('fs');
+var path      = require('path');
 var requirejs = require('requirejs');
 
 //node r.js -o baseUrl=. paths.jquery=some/other/jquery name=main out=main-built.js
 
-module.exports = function (aws, options) {
+module.exports = function (options) {
   return es.mapSync(function (file, cb) {
-      var tmpPath = file.path.replace(file.base, '/tmp/');
-      var stream = fs.createWriteStream(tmpPath, {flags: 'w'});
-      stream.write(file.contents);
-
-      
-
-      fs.unlink(tmpPath);
-  });
+      var destPath = path.join(file.cwd, options.baseUrl, path.basename(file.path));
+      var stream = fs.createWriteStream(destPath, {flags: 'w'});
+      stream.write(file.contents, '', function() {
+        var name = path.basename(file.path).replace(path.extname(file.path), '');
+        var outPath = path.join(options.baseUrl, name + '.js');
+        console.log("node r.js -o baseUrl=" + options.baseUrl + " name=" + name + " out=" + outPath);
+        requirejs.optimize({name:name, baseUrl:options.baseUrl, out:outPath});
+      });
+    });
 };
