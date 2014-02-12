14bcb56ad581b776e74889c30378e70824720a48
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-02-12 11:26:02 +0000

    about to start requirejs integration

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
-var es = require('event-stream');
-var fs = require('fs');
+var es      = require('event-stream');
+var fs      = require('fs');
+var requirejs = require('requirejs');
+
+//node r.js -o baseUrl=. paths.jquery=some/other/jquery name=main out=main-built.js
 
 module.exports = function (aws, options) {
   return es.mapSync(function (file, cb) {
-      var isFile = fs.lstatSync(file.path).isFile();
-      if (!isFile) { return false; }
-      console.log(file.path);
+      var tmpPath = file.path.replace(file.base, '/tmp/');
+      var stream = fs.createWriteStream(tmpPath, {flags: 'w'});
+      stream.write(file.contents);
+
+      
+
+      fs.unlink(tmpPath);
   });
 };
